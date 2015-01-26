e60df09e816c1e47219ea356f7439702a17ade76
Valentin Waeselynck <val@bandsquare.fr>
2015-01-26 22:04:44 +0100

    Implemented step 4, demonstrating the use of reagent cursors

--- project.clj
+++ project.clj
@@ -12,6 +12,7 @@
                  ;[reagent-forms "0.4.3"]
                  ;[reagent-utils "0.1.2"]
                  ;[secretary "1.2.1"]
+                 [reagent/reagent-cursor "0.1.2"]
                  [org.clojure/clojurescript "0.0-2719" :scope "provided"]
                  [com.cemerick/piggieback "0.1.5"]
                  [weasel "0.5.0"]
--- src/cljs/reagent_phonecat/core.cljs
+++ src/cljs/reagent_phonecat/core.cljs
@@ -1,5 +1,6 @@
 (ns reagent-phonecat.core
-    (:require [reagent.core :as rc :refer [atom]]
+    (:require [reagent.core :as rg :refer [atom]]
+              [reagent.cursor :as rc]
               [clojure.string :as str]
               )
     )
@@ -20,23 +21,35 @@ Checks that any string value of the 'data' map matches the 'query' string."
 ;; State
 
 (def state
-  (rc/atom
+  (rg/atom
    {:phones [{:name "Nexus S"
-              :snippet "Fast just got faster with Nexus S."}
+              :snippet "Fast just got faster with Nexus S."
+              :age 1}
              {:name "Motorola XOOM™ with Wi-Fi"
-              :snippet "The Next, Next Generation tablet."}
+              :snippet "The Next, Next Generation tablet."
+              :age 2}
              {:name "MOTOROLA XOOM™"
-              :snippet "The Next, Next Generation tablet."}]
-    :query ""
+              :snippet "The Next, Next Generation tablet."
+              :age 3}]
     }))
 
 ;; -------------------------
 ;; Views
 
-(defn query-input []
-  [:input {:type text
-           :value (:query @state)
-           :on-change #(swap! state assoc :query (-> % .-target .-value))}])
+(defn cursor-input "Generic input field that stays synchronized with the provided reagent cursor."
+  ([opts cursor]
+   [:input (merge {:type "text" 
+                   :value @cursor
+                   :on-change #(reset! cursor (-> % .-target .-value))}
+                  opts)])
+  ([cursor] (cursor-input {} cursor)))
+
+(defn order-select [cursor]
+  [:select {:value @cursor
+            :on-change #(reset! cursor (-> % .-target .-value keyword))}
+   [:option {:value :name} "Alphabetical"]
+   [:option {:value :age} "Newest"]
+   ])
 
 (defn phone-component "component showing details about one phone"
   [{:keys [name snippet] :as phone}]
@@ -45,29 +58,43 @@ Checks that any string value of the 'data' map matches the 'query' string."
    [:p snippet]]
   )
 
-(defn phones-list "Component displaying the list of phones" [query phones]
+(defn phones-list "Component displaying the list of phones"
+  [{:keys [query order-fn] :as diplay-opts} phones]
   [:ul
-   (for [phone (filter (partial matches-query? query) phones)]
-     [phone-component phone])])
+   (for [phone (->> phones
+                    (filter (partial matches-query? query))
+                    (sort-by order-fn))]
+     ^{:key phone} [phone-component phone])])
 
+(defn phones-page "Component containing the display options and phone list, featuring local UI state." []
+  (let [display-state (rg/atom {:query ""    ;; query text to match against 
+                                :order-fn :age ;; property of phones to sort with
+                                })
+        query-cursor (rc/cursor [:query] display-state)
+        order-cursor (rc/cursor [:order-fn] display-state)
+        ]
+    (fn []
+      [:div.row
+       [:div.col-md-2
+        ;; sidebar content
+        "Search: " [cursor-input query-cursor]
+        "Sort by: " [order-select order-cursor] 
+        ]
+       [:div.col-md-10
+        ;; body content
+        [phones-list @display-state (:phones @state)]
+        ]]
+      )))
 
 (defn top-cpnt []
   [:div.container-fluid
-   [:div.row
-    [:div.col-md-2
-     ;; sidebar content
-     "Search : " [query-input]
-     ]
-    [:div.col-md-10
-     ;; body content
-     [phones-list (:query @state) (:phones @state)]
-     ]]
+   [phones-page]
    ])
 ;; -------------------------
 ;; Initialize app
 
 (defn init! []
-  (rc/render-component [top-cpnt] (.getElementById js/document "app")))
+  (rg/render-component [top-cpnt] (.getElementById js/document "app")))
 
 
 ;; -------------------------
